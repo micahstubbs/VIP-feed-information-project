08fa3be2953afb96570f53852fecadfc0fe72bfb
Micah Stubbs <micah.stubbs@gmail.com>
2016-11-06 21:32:15 -0800

    parse.py now can read datafeed.json and parse multiple xml files in series

--- parse.py
+++ parse.py
@@ -8,19 +8,22 @@ with open('2016-general-election-datafeed-no-early-unparsed.json') as json_file:
   datafeed = json.load(json_file)
 
   for d in datafeed:
+    print d
     try:
-      found = re.search('vipfeed(.*?)(?=\.zip",$)', d.feed_url).group(0)
+      found = re.search("vipfeed(.*?)(?=\.zip)", d["feed_url"]).group(0)
     except AttributeError:
       # pattern not found in the original string
       found = '' # apply your error handling
 
     # Create the soup
     directory = found
+    print directory
     directoryPath = 'data/{0}'.format(directory)
     fileName = os.listdir(directoryPath)[0]
     filePath = 'data/{0}/{1}'.format(directory, fileName)
+    print filePath
     
-    with open(file, 'r') as myfile:
+    with open(filePath, 'r') as myfile:
       input = myfile.read().replace('\n', '')
       soup = BeautifulSoup(input)
     
